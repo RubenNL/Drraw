0b3fdd8a28a53e5fa0772feac6f16a84320efd28
Lint Action <lint-action@samuelmeuli.com>
2021-01-16 12:27:51 +0000

    Fix code style issues with Prettier

--- server/Game.js
+++ server/Game.js
@@ -1,107 +1,116 @@
-const grabWords=require('./words')
-module.exports=class Game {
+const grabWords = require('./words')
+module.exports = class Game {
 	constructor(player) {
-		this.players=[]
-		this.drawer=null;
-		this.word='';
-		this.interval='';
+		this.players = []
+		this.drawer = null
+		this.word = ''
+		this.interval = ''
 		this.addPlayer(player)
-		this.replay=[];
-		this.startTimer=0;
+		this.replay = []
+		this.startTimer = 0
 	}
 	addPlayer(player) {
-		player.score=0;
+		player.score = 0
 		this.players.push(player)
-		player.send({id:this.players.indexOf(player)})
-		this.sendPlayerStats();
-		if(this.drawer) this.replay.forEach(action=>player.send({draw:action}))
-		player.on('message',message=>{
-			message=JSON.parse(message)
-			if(message.name) {
-				player.name=message.name;
-				this.sendAll({chat:{from:'GAME',message:player.name+' joined the game!'}})
-				this.sendPlayerStats();
+		player.send({id: this.players.indexOf(player)})
+		this.sendPlayerStats()
+		if (this.drawer) this.replay.forEach(action => player.send({draw: action}))
+		player.on('message', message => {
+			message = JSON.parse(message)
+			if (message.name) {
+				player.name = message.name
+				this.sendAll({chat: {from: 'GAME', message: player.name + ' joined the game!'}})
+				this.sendPlayerStats()
 			}
-			if(message.chat) {
-				if(message.chat==this.word) {
-					if(player.correct) player.send({chat:{from:'GAME',message:'nope. geen gratis punten :)'}})
+			if (message.chat) {
+				if (message.chat == this.word) {
+					if (player.correct) player.send({chat: {from: 'GAME', message: 'nope. geen gratis punten :)'}})
 					else {
-						this.sendAll({chat:{from:'GAME',message:player.name+' heeft het juiste woord geraden!'}})
-						player.score+=this.timer;
-						player.correct=true;
-						player.send({word:this.word})
-						this.sendPlayerStats();
-						if(this.players.every(player=>player.correct)) this.endDraw()
+						this.sendAll({chat: {from: 'GAME', message: player.name + ' heeft het juiste woord geraden!'}})
+						player.score += this.timer
+						player.correct = true
+						player.send({word: this.word})
+						this.sendPlayerStats()
+						if (this.players.every(player => player.correct)) this.endDraw()
 					}
-				} else this.sendAll({chat:{from:player.name,message:message.chat}})
+				} else this.sendAll({chat: {from: player.name, message: message.chat}})
 			}
-			if(message.draw&&this.drawer==player) {
+			if (message.draw && this.drawer == player) {
 				this.replay.push(message.draw)
-				this.sendAll({draw:message.draw})
+				this.sendAll({draw: message.draw})
 			}
-			if(message.word&&this.drawer==player) {
-				this.replay=[];
-				this.word=message.word;
-				this.players.forEach(player=>player.correct=false)
-				this.drawer.correct=true;
-				this.sendAll({action:'clear',word:this.word.split('').map(()=>'_').join(' '),chat:{from:'GAME',message:player.name+' heeft een woord gekozen!'}})
-				this.drawer.send({word:this.word})
-				this.interval=setInterval(()=>{
-					this.timer--;
-					this.sendAll({timer:this.timer})
-					if(this.timer==0) this.endDraw();
-				},1000)
+			if (message.word && this.drawer == player) {
+				this.replay = []
+				this.word = message.word
+				this.players.forEach(player => (player.correct = false))
+				this.drawer.correct = true
+				this.sendAll({
+					action: 'clear',
+					word: this.word
+						.split('')
+						.map(() => '_')
+						.join(' '),
+					chat: {from: 'GAME', message: player.name + ' heeft een woord gekozen!'},
+				})
+				this.drawer.send({word: this.word})
+				this.interval = setInterval(() => {
+					this.timer--
+					this.sendAll({timer: this.timer})
+					if (this.timer == 0) this.endDraw()
+				}, 1000)
 			}
-			if(message.start&&!this.drawer) {
-				this.startTimer=message.start.timer;
-				this.sendAll({action:'start'})
-				this.nextDrawer();
+			if (message.start && !this.drawer) {
+				this.startTimer = message.start.timer
+				this.sendAll({action: 'start'})
+				this.nextDrawer()
 			}
-			switch(message.gameAction) {
+			switch (message.gameAction) {
 				case 'clear':
-					if(this.drawer!=player) return
-					this.sendAll({action:'clear'})
-					this.replay=[];
-					break;
+					if (this.drawer != player) return
+					this.sendAll({action: 'clear'})
+					this.replay = []
+					break
 			}
 		})
-		player.on('close',()=>{
-			this.players.splice(this.players.indexOf(player), 1);
-			this.sendAll({chat:{from:'GAME',message:player.name+' left the game!'}})
-			this.sendPlayerStats();
-			if(player==this.drawer) this.nextDrawer();
+		player.on('close', () => {
+			this.players.splice(this.players.indexOf(player), 1)
+			this.sendAll({chat: {from: 'GAME', message: player.name + ' left the game!'}})
+			this.sendPlayerStats()
+			if (player == this.drawer) this.nextDrawer()
 		})
 	}
 	nextDrawer() {
-		clearTimeout(this.interval);
-		if(this.players.length==0) {
-			this.delete();
-			return;
+		clearTimeout(this.interval)
+		if (this.players.length == 0) {
+			this.delete()
+			return
 		}
-		let drawerId=this.players.indexOf(this.drawer)+1;
-		if(!this.players[drawerId]) drawerId=0;
-		this.drawer=this.players[drawerId]
-		this.drawer.send({words:grabWords(3)})
-		this.timer=this.startTimer;
-		this.sendAll({timer:this.drawer.name+' Kiest een woord...',chat:{from:'GAME',message:this.drawer.name+' Kiest een woord...'}})
-		this.sendPlayerStats();
+		let drawerId = this.players.indexOf(this.drawer) + 1
+		if (!this.players[drawerId]) drawerId = 0
+		this.drawer = this.players[drawerId]
+		this.drawer.send({words: grabWords(3)})
+		this.timer = this.startTimer
+		this.sendAll({timer: this.drawer.name + ' Kiest een woord...', chat: {from: 'GAME', message: this.drawer.name + ' Kiest een woord...'}})
+		this.sendPlayerStats()
 	}
 	endDraw() {
-		clearInterval(this.interval);
-		this.sendAll({action:'endDraw',word:this.word})
+		clearInterval(this.interval)
+		this.sendAll({action: 'endDraw', word: this.word})
 		this.nextDrawer()
 	}
 	sendPlayerStats() {
-		this.sendAll({players:this.players.map(player=>{
-			return {
-				name:player.name,
-				score:player.score,
-				correct:player.correct,
-				drawer:player==this.drawer
-			}
-		})})
+		this.sendAll({
+			players: this.players.map(player => {
+				return {
+					name: player.name,
+					score: player.score,
+					correct: player.correct,
+					drawer: player == this.drawer,
+				}
+			}),
+		})
 	}
 	sendAll(message) {
-		this.players.forEach(player=>player.send(message))
+		this.players.forEach(player => player.send(message))
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,163 +1,173 @@
 import dialogPolyfill from 'dialog-polyfill'
 import flood from './flood.js'
-import 'fa-icons';
-const dialog = document.querySelector('dialog');
-dialogPolyfill.registerDialog(dialog);
-const gameId=location.hash.split('#')[1]||prompt('gameId?',Math.random().toString(36).substring(7))
-if(!gameId) {
+import 'fa-icons'
+const dialog = document.querySelector('dialog')
+dialogPolyfill.registerDialog(dialog)
+const gameId = location.hash.split('#')[1] || prompt('gameId?', Math.random().toString(36).substring(7))
+if (!gameId) {
 	alert('geen game ID!')
 	throw new Error()
 }
-location.hash='#'+gameId
-const name=window.sessionStorage.getItem('name')||prompt('naam?')
-if(!name) {
+location.hash = '#' + gameId
+const name = window.sessionStorage.getItem('name') || prompt('naam?')
+if (!name) {
 	alert('geen naam!')
 	throw new Error()
 }
-window.sessionStorage.setItem('name',name)
-document.querySelector('#color').innerHTML=['red','blue','green','yellow','black'].map(color=>`<label style="background-color: ${color}"><input name="color" type="radio" value="${color}"></label>`).join('')
-document.querySelector('#width').innerHTML=[1,2,3,4,5,10,20,50,100,200,400].map(width=>`<option ${width==20?"selected":""} value="${width}">${width}</option>`).join('')
-document.querySelector('#start').onclick=()=>{
-	const timer=parseInt(prompt('seconde per beurt?'))
-	if(!timer||timer<5) {
+window.sessionStorage.setItem('name', name)
+document.querySelector('#color').innerHTML = ['red', 'blue', 'green', 'yellow', 'black'].map(color => `<label style="background-color: ${color}"><input name="color" type="radio" value="${color}"></label>`).join('')
+document.querySelector('#width').innerHTML = [1, 2, 3, 4, 5, 10, 20, 50, 100, 200, 400].map(width => `<option ${width == 20 ? 'selected' : ''} value="${width}">${width}</option>`).join('')
+document.querySelector('#start').onclick = () => {
+	const timer = parseInt(prompt('seconde per beurt?'))
+	if (!timer || timer < 5) {
 		alert('Ongeldige tijd!')
 		return
 	}
-	ws.send({start:{timer}})
+	ws.send({start: {timer}})
 }
-document.querySelector('#clear').onclick=()=>ws.send({gameAction:'clear'})
-document.querySelector('#chatInput').onkeydown=event=>{
-	if(event.key!='Enter') return;
-	ws.send({chat:document.querySelector('#chatInput').value})
-	document.querySelector('#chatInput').value='';
+document.querySelector('#clear').onclick = () => ws.send({gameAction: 'clear'})
+document.querySelector('#chatInput').onkeydown = event => {
+	if (event.key != 'Enter') return
+	ws.send({chat: document.querySelector('#chatInput').value})
+	document.querySelector('#chatInput').value = ''
 }
-let id=0;
-const ws=new WebSocket((location.protocol=="http:"?'ws://':'wss://')+(location+'').split('/')[2]+'/?game='+gameId);
-ws.oldSend=ws.send;
-ws.send=message=>ws.oldSend(JSON.stringify(message))
-ws.addEventListener('open', ()=>{
+let id = 0
+const ws = new WebSocket((location.protocol == 'http:' ? 'ws://' : 'wss://') + (location + '').split('/')[2] + '/?game=' + gameId)
+ws.oldSend = ws.send
+ws.send = message => ws.oldSend(JSON.stringify(message))
+ws.addEventListener('open', () => {
 	ws.send({name})
-	setInterval(()=>ws.send({}),45000) //keepalive for heroku: https://devcenter.heroku.com/articles/http-routing#timeouts
-});
-ws.addEventListener('close',event=>{
-	if(event.reason) alert('verbinding verbroken:\n'+event.reason)
+	setInterval(() => ws.send({}), 45000) //keepalive for heroku: https://devcenter.heroku.com/articles/http-routing#timeouts
+})
+ws.addEventListener('close', event => {
+	if (event.reason) alert('verbinding verbroken:\n' + event.reason)
 	else alert('verbinding verbroken, geen reason opgegeven.')
 })
-let drawer=false;
-ws.addEventListener('message',event=>{
-	const data=JSON.parse(event.data)
-	if(data.id) id=data.id;
-	if(data.draw) draw(data.draw)
-	if(data.words) {
-		dialog.innerHTML=data.words.map(word=>`<button onclick="wordClick('${word}')">${word}</button>`).join('')
-		dialog.showModal();
+let drawer = false
+ws.addEventListener('message', event => {
+	const data = JSON.parse(event.data)
+	if (data.id) id = data.id
+	if (data.draw) draw(data.draw)
+	if (data.words) {
+		dialog.innerHTML = data.words.map(word => `<button onclick="wordClick('${word}')">${word}</button>`).join('')
+		dialog.showModal()
 	}
-	if(data.word) document.querySelector('#word').innerText=data.word
-	if(data.timer) document.querySelector('#timer').innerText=data.timer
-	if(data.chat) {
-		const chatDiv=document.querySelector('#chat')
-		chatDiv.innerHTML+=`<span><b>${data.chat.from}</b>: ${data.chat.message}</b></span><br>`
-		chatDiv.scrollTop = chatDiv.scrollHeight;
+	if (data.word) document.querySelector('#word').innerText = data.word
+	if (data.timer) document.querySelector('#timer').innerText = data.timer
+	if (data.chat) {
+		const chatDiv = document.querySelector('#chat')
+		chatDiv.innerHTML += `<span><b>${data.chat.from}</b>: ${data.chat.message}</b></span><br>`
+		chatDiv.scrollTop = chatDiv.scrollHeight
 	}
-	if(data.players) {
-		document.querySelector('#players').innerHTML=data.players.map(player=>{
-			if(player.id==id) drawer=player.drawer;
-			return `
-			<div class="player ${drawer?"me":"other"} ${player.correct?"correct":"incorrect"} ${player.drawer?"drawer":"notdrawing"}">
+	if (data.players) {
+		document.querySelector('#players').innerHTML = data.players
+			.map(player => {
+				if (player.id == id) drawer = player.drawer
+				return `
+			<div class="player ${drawer ? 'me' : 'other'} ${player.correct ? 'correct' : 'incorrect'} ${player.drawer ? 'drawer' : 'notdrawing'}">
 				name: ${player.name}<br>
 				score: ${player.score}<br>
 			</div>
-		`}).join('')
+		`
+			})
+			.join('')
 	}
-	switch(data.action) {
+	switch (data.action) {
 		case 'start':
-			document.querySelector('#start').disabled=true;
-			break;
+			document.querySelector('#start').disabled = true
+			break
 		case 'clear':
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			break;
+			ctx.clearRect(0, 0, canvas.width, canvas.height)
+			break
 		case 'endDraw':
-			break;
+			break
 	}
-});
-window.wordClick=word=>{
-	ws.send({word});
-	dialog.close();
+})
+window.wordClick = word => {
+	ws.send({word})
+	dialog.close()
 }
-document.querySelector('#chat').style.height=document.querySelector('canvas').height-document.querySelector('#chatInput').offsetHeight	;
-document.querySelector('#players').style.height=document.querySelector('canvas').height;
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-let pos = { x: 0, y: 0 };
-canvas.addEventListener('mousemove', send);
-canvas.addEventListener('touchmove', e=>send({buttons:1,clientX:e.touches[0].clientX,clientY:e.touches[0].clientY}));
-canvas.addEventListener('touchstart', e=>setPosition({clientX:e.touches[0].clientX,clientY:e.touches[0].clientY}));
-canvas.addEventListener('mousedown', setPosition);
-canvas.addEventListener('mouseenter', setPosition);
-canvas.addEventListener('click',click);
+document.querySelector('#chat').style.height = document.querySelector('canvas').height - document.querySelector('#chatInput').offsetHeight
+document.querySelector('#players').style.height = document.querySelector('canvas').height
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+let pos = {x: 0, y: 0}
+canvas.addEventListener('mousemove', send)
+canvas.addEventListener('touchmove', e => send({buttons: 1, clientX: e.touches[0].clientX, clientY: e.touches[0].clientY}))
+canvas.addEventListener('touchstart', e => setPosition({clientX: e.touches[0].clientX, clientY: e.touches[0].clientY}))
+canvas.addEventListener('mousedown', setPosition)
+canvas.addEventListener('mouseenter', setPosition)
+canvas.addEventListener('click', click)
 function setPosition(evt) {
-	if (evt.buttons !== 1) return;
+	if (evt.buttons !== 1) return
 	const rect = canvas.getBoundingClientRect()
-	const scaleX = canvas.width / rect.width;
-	const scaleY = canvas.height / rect.height;
-	pos={
+	const scaleX = canvas.width / rect.width
+	const scaleY = canvas.height / rect.height
+	pos = {
 		x: (evt.clientX - rect.left) * scaleX,
-		y: (evt.clientY - rect.top) * scaleY
+		y: (evt.clientY - rect.top) * scaleY,
 	}
-	
 }
 function click(evt) {
-	if(!drawer) return
+	if (!drawer) return
 	setPosition(evt)
-	let color=document.querySelector('[name="color"]:checked').value;
-	if(document.querySelector('#erase').checked) color='white'
-	if(document.querySelector('#flood').checked) ws.send({draw:{
-		action:'flood',
-		color,
-		pos
-	}})
-	else ws.send({draw:{
-		action:'dot',
-		color,
-		width:document.querySelector('#width').value,
-		pos
-	}})
+	let color = document.querySelector('[name="color"]:checked').value
+	if (document.querySelector('#erase').checked) color = 'white'
+	if (document.querySelector('#flood').checked)
+		ws.send({
+			draw: {
+				action: 'flood',
+				color,
+				pos,
+			},
+		})
+	else
+		ws.send({
+			draw: {
+				action: 'dot',
+				color,
+				width: document.querySelector('#width').value,
+				pos,
+			},
+		})
 }
 function send(e) {
-	if(!drawer) return;
-	if (e.buttons !== 1) return;
-	if(document.querySelector('#flood').checked) return
-	let color=document.querySelector('[name="color"]:checked').value;
-	if(document.querySelector('#erase').checked) color='white'
-	const first=JSON.parse(JSON.stringify(pos));
-	setPosition(e);
-	const second=pos;
-	ws.send({draw:{
-		action:'line',
-		first:first,
-		second:second,
-		color:color,
-		width:document.querySelector('#width').value
-	}})
+	if (!drawer) return
+	if (e.buttons !== 1) return
+	if (document.querySelector('#flood').checked) return
+	let color = document.querySelector('[name="color"]:checked').value
+	if (document.querySelector('#erase').checked) color = 'white'
+	const first = JSON.parse(JSON.stringify(pos))
+	setPosition(e)
+	const second = pos
+	ws.send({
+		draw: {
+			action: 'line',
+			first: first,
+			second: second,
+			color: color,
+			width: document.querySelector('#width').value,
+		},
+	})
 }
-function draw({action,...data}) {
+function draw({action, ...data}) {
 	console.log(action)
-	if(action=='flood') flood(data,canvas)
-	if(action=="line") drawLine(data)
-	if(action=="dot") drawDot(data)
+	if (action == 'flood') flood(data, canvas)
+	if (action == 'line') drawLine(data)
+	if (action == 'dot') drawDot(data)
 }
-function drawLine({first,second,color,width}) {
-	ctx.beginPath();
-	ctx.lineWidth = width;
-	ctx.lineCap = 'round';
-	ctx.strokeStyle = color;
-	ctx.moveTo(first.x, first.y);
-	ctx.lineTo(second.x, second.y);
-	ctx.stroke();
+function drawLine({first, second, color, width}) {
+	ctx.beginPath()
+	ctx.lineWidth = width
+	ctx.lineCap = 'round'
+	ctx.strokeStyle = color
+	ctx.moveTo(first.x, first.y)
+	ctx.lineTo(second.x, second.y)
+	ctx.stroke()
 }
-function drawDot({pos,width,color}) {
-	ctx.beginPath();
-	ctx.arc(pos.x,pos.y,width/2,2*Math.PI,false)
-	ctx.fillStyle=color;
-	ctx.fill();
+function drawDot({pos, width, color}) {
+	ctx.beginPath()
+	ctx.arc(pos.x, pos.y, width / 2, 2 * Math.PI, false)
+	ctx.fillStyle = color
+	ctx.fill()
 }
