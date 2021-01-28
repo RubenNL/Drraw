f990bfd74eaa8c947bf632d7b8fb174cf9760ae5
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-01-28 12:30:04 +0100

    wordsAutoDownloadToDB

--- rollup.config.js
+++ rollup.config.js
@@ -16,6 +16,7 @@ export default {
 	},
 	watch: {
 		exclude: 'node_modules/**, server/**',
+		clearScreen: false,
 	},
 	preserveEntrySignatures: 'allow-extension',
 	plugins: [
--- server/words.js
+++ server/words.js
@@ -1,6 +1,45 @@
-const woorden = JSON.parse(require('fs').readFileSync('woorden.json', 'utf8'))
+const {Sequelize, DataTypes} = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL ? process.env.DATABASE_URL : 'sqlite:data.db')
+const word = sequelize.define('word', {word: DataTypes.STRING})
+sequelize.sync()
+let totalWords = 0
+function download() {
+	return new Promise(resolve => {
+		const https = require('https')
+		const url = 'https://nl.wiktionary.org/w/api.php?action=query&list=search&srsearch=incategory:%22Prevalentie%20Nederland%2099%20%%22%20incategory:%22Zelfstandig%20naamwoord%20in%20het%20Nederlands%22&format=json&srlimit=500&srsort=create_timestamp_asc'
+		let words = []
+		function nextDownload() {
+			https.get(url + '&sroffset=' + words.length, res => {
+				let data = ''
+				res.on('data', chunk => (data += chunk))
+				res.on('end', () => {
+					data = JSON.parse(data).query.search.map(item => item.title)
+					words.push(...data)
+					if (data.length == 500) nextDownload()
+					else resolve(words)
+				})
+			})
+		}
+		nextDownload()
+	})
+}
+word.count().then(count => {
+	if (count == 0) {
+		return download().then(words => {
+			totalWords = words.length
+			return word.bulkCreate(
+				words.map(word => {
+					return {word}
+				})
+			)
+		})
+	} else {
+		totalWords = count
+		return Promise.resolve()
+	}
+})
 module.exports = aantal => {
 	let response = []
-	for (let i = 0; i < aantal; i++) response.push(woorden[Math.floor(Math.random() * woorden.length)])
-	return response
+	for (let i = 0; i < aantal; i++) response.push(word.findByPk(Math.floor(Math.random() * totalWords)))
+	return Promise.all(response).then(words => words.map(word => word.word))
 }
